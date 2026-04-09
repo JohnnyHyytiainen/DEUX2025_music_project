87eab42be0739b6b110da2f9157a16ddef6a209b
Johnny Hyytiäinen <johnny.hyytiainen@gmail.com>
2026-04-09 11:02:36 +0200

    Feat/db ingestion rik anj (#7)
    * feat(date): updated date time in anjas set
    
    * feat(DB+standardise): standardised datatypes, changed names on columns to match with everything else, set up database with enteties for 30m rows
    
    * docs(tracking): updated session tracking notes
    
    * chore: removed unnecessary notebook file
    
    * chore(name): updated naming of file

--- src/init_db.py
+++ src/init_db.py
@@ -9,7 +9,11 @@ import os
 # KONSTANTER RÖR EJ
 # ==========================================
 DB_PATH = "../data/music_warehouse.duckdb"
-PARQUET_PATH = "../data/processed/spotify_daily_top_songs_clean_version608.parquet"
+PARQUET_PATH_DAILY = (
+    "../data/processed/spotify_daily_top_songs_clean_version608.parquet"
+)
+PARQUET_PATH_CHARTS = "../data/processed/spotify_historical_charts.parquet"
+PARQUET_PATH_TOP = "../data/processed/spotify_top_200_historical.parquet"
 
 
 def init_database():
@@ -24,20 +28,48 @@ def init_database():
     # IDEMPOTENS: 'CREATE OR REPLACE TABLE' raderar den gamla tabellen om den finns
     # och bygger upp den på nytt med den senaste Parquet-datan.
     # ==========
-    query = f"""
+    query_a = f"""
     CREATE OR REPLACE TABLE silver_spotify_daily AS 
-    SELECT * FROM '{PARQUET_PATH}';
+    SELECT * FROM '{PARQUET_PATH_DAILY}';
+    """
+
+    query_b = f""" 
+    CREATE OR REPLACE TABLE silver_historical_charts AS
+    SELECT * FROM '{PARQUET_PATH_CHARTS}';
+    """
+
+    query_c = f"""
+    CREATE OR REPLACE TABLE silver_top_200_historical AS
+    SELECT * FROM '{PARQUET_PATH_TOP}'
     """
 
     try:
         print("Reading in .Parquet file and building table. This will go QUICK..")
-        con.execute(query)
+        con.execute(query_a)
+        con.execute(query_b)
+        con.execute(query_c)
 
         # Validering: Kolla hur många rader som faktiskt gick in
-        count = con.execute("SELECT COUNT(*) FROM silver_spotify_daily").fetchone()[0]
+        count_a = con.execute("SELECT COUNT(*) FROM silver_spotify_daily").fetchone()[0]
 
         print("SUCCESS!")
-        print(f"Table 'silver_spotify_daily' is created with: {count} rows.")
+        print(f"Table 'silver_spotify_daily' is created with: {count_a} rows.")
+
+        # Validering: Kolla hur många rader som faktiskt gick in
+        count_b = con.execute(
+            "SELECT COUNT(*) FROM silver_historical_charts"
+        ).fetchone()[0]
+
+        print("SUCCESS!")
+        print(f"Table 'silver_historical_charts' is created with: {count_b} rows.")
+
+        # Validering: Kolla hur många rader som faktiskt gick in
+        count_c = con.execute(
+            "SELECT COUNT (*) FROM silver_top_200_historical"
+        ).fetchone()[0]
+
+        print("SUCCESS!")
+        print(f"Table 'silver_top_200_historical' is created with: {count_c} rows.")
 
     except Exception as e:
         print(f"Error occured while setting up database: {e}")
