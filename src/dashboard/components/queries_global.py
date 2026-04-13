04ecba20cd2a615faf5281be9a1287258ca8ea75
Johnny Hyytiäinen <Johnny.Hyytiainen@gmail.com>
2026-04-13 17:09:16 +0200

    feat(streamlit): updated repo with PoC streamlit app. Works as intended with placeholder dim table for country ISO codes. WILL FIX proper solution

---
+++ src/dashboard/components/queries_global.py
@@ -0,0 +1,53 @@
+# Script för att göra queries som ska synas på 01_🌍_Global_EDA.py sidan.
+# Kommentarer: Svenska
+# Kod: Engelska
+
+
+def get_top_explicit_query(region: str) -> str:
+    """Returns query to get countries with most explicit music taste."""
+
+    # En tillfällig 'Dimensionstabell' i Python
+    region_map = {
+        "Norden": "('SE', 'NO', 'DK', 'FI', 'IS')",
+        "Nordamerika": "('US', 'CA')",
+        "Latinamerika": "('BR', 'MX', 'AR', 'CL', 'CO')",
+        "Asien": "('JP', 'KR', 'TW', 'HK', 'SG')",
+    }
+
+    # Bygga WHERE clause dynamiskt
+    where_clause = "WHERE country != 'Global'"
+
+    # Om användaren valt en specifik region (inte "Alla")
+    if region in region_map:
+        where_clause += f" AND country IN {region_map[region]}"
+
+    # Skicka in den dynamiska WHERE satsen i SQL str
+    return f"""
+    SELECT 
+        country,
+        AVG(CAST(is_explicit AS INT)) * 100 as Explicit_Procent,
+        AVG(tempo) as Avg_BPM
+    FROM silver_spotify_daily
+    {where_clause}
+    GROUP BY country
+    HAVING COUNT(*) > 100
+    ORDER BY Explicit_Procent DESC
+    LIMIT 10;
+    """
+
+
+# Här efter kan vi lägga till flera queries som vi vill visa på sida 1 av vår dashboard.
+# def get_vad_som_helst / def kings_of_valence / def ...... ....
+def get_mood_and_tempo_query() -> str:
+    """Returns query to get both Valence and tempo per Country"""
+    return """
+    SELECT 
+        country,
+        AVG(valence) * 100 as happiness_score,
+        AVG(tempo) as avg_bpm,
+        COUNT(DISTINCT spotify_id) as unique_songs_played
+    FROM silver_spotify_daily
+    WHERE country != 'Global'
+    GROUP BY country
+    HAVING unique_songs_played > 100
+    """
