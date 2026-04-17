fd546e49c45f45d6a3d9f7a960d89bcbed1fd8b2
Johnny Hyytiäinen <Johnny.Hyytiainen@gmail.com>
2026-04-17 12:27:43 +0200

    Chore(rolled back to old init db file): Rollback to original init_db script. DO NOT WORK IN MAIN! SORRY! QUICK FIX!

--- src/init_db.py
+++ src/init_db.py
@@ -30,15 +30,58 @@ def init_database():
     # och bygger upp den på nytt med den senaste Parquet-datan.
     # ==========
 
+    query_a = f"""
+    CREATE OR REPLACE TABLE silver_spotify_daily AS 
+    SELECT * FROM '{PARQUET_PATH_DAILY}';
+    """
+    query_b = f""" 
+    CREATE OR REPLACE TABLE silver_historical_charts AS
+    SELECT * FROM '{PARQUET_PATH_CHARTS}';
+    """
+    query_c = f"""
+    CREATE OR REPLACE TABLE silver_top_200_historical AS
+    SELECT * FROM '{PARQUET_PATH_TOP}'
+    """
+
     query_d = f"""
     CREATE OR REPLACE TABLE silver_music_format_sales AS
     SELECT * FROM '{PARQUET_PATH_SALES}'
+    """
+    query_e = f"""
+    CREATE OR REPLACE TABLE dim_geography AS
+    SELECT * FROM '{PARQUET_PATH_DIM_TABLE}'
     """
 
     try:
         print("Reading in .Parquet file and building table. This will go QUICK..")
 
+        con.execute(query_a)
+        con.execute(query_b)
+        con.execute(query_c)
         con.execute(query_d)
+        con.execute(query_e)
+
+        # Validering: Kolla hur många rader som faktiskt gick in
+        count_a = con.execute("SELECT COUNT(*) FROM silver_spotify_daily").fetchone()[0]
+
+        print("SUCCESS!")
+        print(f"Table 'silver_spotify_daily' is created with: {count_a} rows.")
+
+        # Validering: Kolla hur många rader som faktiskt gick in
+        count_b = con.execute(
+            "SELECT COUNT(*) FROM silver_historical_charts"
+        ).fetchone()[0]
+
+        print("SUCCESS!")
+        print(f"Table 'silver_historical_charts' is created with: {count_b} rows.")
+
+        # Validering: Kolla hur många rader som faktiskt gick in
+        count_c = con.execute(
+            "SELECT COUNT (*) FROM silver_top_200_historical"
+        ).fetchone()[0]
+
+        print("SUCCESS!")
+        print(f"Table 'silver_top_200_historical' is created with: {count_c} rows.")
 
         # Validering: Kolla hur många rader som faktiskt gick in
         count_d = con.execute(
