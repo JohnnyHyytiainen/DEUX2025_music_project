c962caeab69a19b9e478973af8671e38a7696926
Anja Scherwall <anjagutt@hotmail.com>
2026-05-06 13:45:00 +0200

    change layout and create linechart with possibility of two lines/countries

--- src/dashboard/components/charts/charts_streams.py
+++ src/dashboard/components/charts/charts_streams.py
@@ -1,18 +1,26 @@
 import plotly.express as px
 from components.data_loader import fetch_data
-from components.queries.queries_historical import line_chart_filter
+from components.queries.queries_historical import line_chart_query
 
 
 
-def streams_over_time_line_chart(selected_cont, selected_country, start_date, end_date):
+def streams_over_time_line_chart(selected_cont, selected_countries, start_date, end_date):
     """Creante a linechart to show streams over time in different countries"""
-    df = fetch_data(line_chart_filter(selected_cont, selected_country, start_date, end_date))
+    df = fetch_data(line_chart_query(selected_cont, selected_countries, start_date, end_date))
 
-    fig = px.line(
-        df,
-        x="Date",
-        y="Streams"
-    )
+    if "Country" in df.columns:
+        fig = px.line(
+            df,
+            x="Date",
+            y="Streams",
+            color="Country",
+        )
+    else:
+        fig = px.line(
+            df,
+            x="Date",
+            y="Streams"
+        )
 
     return fig
 
--- src/dashboard/components/filter/filters_streams.py
+++ src/dashboard/components/filter/filters_streams.py
@@ -1,12 +1,45 @@
 import streamlit as st
 
 
-def continent_filter(continents_list):
-    return st.selectbox(label="Filter by continent", options=continents_list)
+def continent_filter(continents):
+    col = st.columns(3)
+    with col[2]:
+        if st.button("Europe", width=700):
+            st.session_state.continent = "Europe"
+    with col[0]:
+        if st.button("Asia", width=700):
+            st.session_state.continent = "Asia"
+    with col[1]:
+        if st.button("Africa", width=700):
+            st.session_state.continent = "Africa"
+    with col[2]:
+        if st.button("Oceania", width=700):
+            st.session_state.continent = "Oceania"
+    with col[0]:
+        if st.button("North America", width=700):
+            st.session_state.continent = "North America"
+    with col[1]:
+        if st.button("South America", width=700):
+            st.session_state.continent = "South America"
+
+    return st.session_state.continent
+
 
 
 def country_filter(countries_list):
-    return st.selectbox(label="Filter by country", options=countries_list)
+    col1, col2 = st.columns(2)
+    with col1:
+        country1 = st.selectbox(label="Filter by country", options=countries_list)
+    with col2:
+        country2 = st.selectbox(label="Select another country to compare", options=countries_list)
+
+    selected_countries = []
+    if country1 and country1 !="Alla":
+        selected_countries.append(country1)
+    if country2 and country2 !="Alla" and country2 != country1:
+        selected_countries.append(country2)
+
+    return selected_countries
 
 
 def date_filter(date_list):
